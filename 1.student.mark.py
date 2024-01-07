dc2b3b7b210324af1196460c00c5c8e03e5f6f3b
HoangPham4171 <52208311+HoangPham6337@users.noreply.github.com>
2024-01-07 21:39:40 +0700

    Add addition comment to the code for clearer explanation

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,7 +1,7 @@
 import os
 # Utility functions
 
-def formatted_input(message, option):
+def formatted_input(message, option):  # Function to reduce clutter in code
     user_input = input(message).strip()
     if option == 1:
         user_input = user_input.upper()
@@ -11,24 +11,24 @@ def formatted_input(message, option):
         user_input = user_input.capitalize()
     return user_input
 
-def check_null(input_string):
+def check_null(input_string):  # Simple function to check if the input is NULL
     if input_string == "":
         return True
     return False
 
-def check_duplicate_in_list(check_case, list_to_search):
+def check_duplicate_in_list(check_case, list_to_search):  # Search the entire list to check for duplication key or key's value
     for item in list_to_search:
         for key in item.keys():
             if key == check_case or item[key] == check_case:
                 return True
         return False
 
-def is_class_full(class_to_check):
+def is_class_full(class_to_check):  # Figure out in the class dictionary if the class is full or not
     if class_to_check["Current"] < class_to_check["Total"]:
         return False
     return True
 
-def find_dict_in_list(key, dict_to_find, list_to_search):
+def find_dict_in_list(key, dict_to_find, list_to_search):  # Find the dictionary in a list that has a specific key
     for i in range(0, len(list_to_search)):
         if list_to_search[i][key] == dict_to_find:
             return i
@@ -49,8 +49,8 @@ def add_class(class_list):
         "ID": class_id,
         "Total": number_of_student,
         "Current": 0,
-        "Student list": [],
-        "Mark list": []
+        "Student list": [],  # Hold the student in a class here, will be reference later
+        "Mark list": []  # Hold the student's mark here, will be reference later -> Not the best way to implement this
     }
     class_list.append(new_class) 
 
@@ -71,8 +71,8 @@ def add_student(student_list, class_list):
         "ID": student_id,
         "DOB": student_dob,
     }
-    add_student_to_class(student, class_list)
-    student_list.append(student)
+    add_student_to_class(student, class_list)  # Add student to class here
+    student_list.append(student)  # Add student to the global student list
 
 def add_student_to_class(student, class_list):
     show_all_class(class_list)
@@ -81,20 +81,21 @@ def add_student_to_class(student, class_list):
     if check_null(class_to_add):
         return
 
+    # Figure out if the class exist
     class_position = find_dict_in_list("ID", class_to_add, class_list)
     if class_position != -1:
-        if is_class_full(class_list[class_position]):
+        if is_class_full(class_list[class_position]):  # Check if the class is full, if yes -> ask the user if they want to increase total student count
             if formatted_input(f"The class is full, do you want to increase the total number of student in {class_to_add} (Y/N): ", 1) == 'Y':
                 class_list[class_position]["Total"] += 1
             else:
                 input(f"Failed to add to {class_to_add}, please choose another class. Press Enter to continue.")
                 return
-        class_list[class_position]["Student list"].append(student["ID"])
-        class_list[class_position]["Current"] += 1
+        class_list[class_position]["Student list"].append(student["ID"])  # Add the ID to the student list -> reference later
+        class_list[class_position]["Current"] += 1  # Increase the current student count
     else:
         input("Class not found, class set to None. Press Enter to continue.")
 
-def add_student_class_standalone(student_list, class_list):
+def add_student_class_standalone(student_list, class_list):  # Add class to existing student -> facilitate multiple classes per student
     student = formatted_input("Enter student ID: ", 1)
     if find_dict_in_list("ID", student, student_list) != -1:
         add_student_to_class({"ID": student}, class_list)
@@ -111,15 +112,16 @@ def add_mark(student_list, class_list):
     student_position = find_dict_in_list("ID", student, student_list)
     if student_position == -1:
         input("Student not found, press Enter to try again.")
+
     mark = input("Enter student mark: ")
-    class_list[class_position]["Mark list"].append({student: mark})
+    class_list[class_position]["Mark list"].append({student: mark})  # Add the dictionary with student's ID and mark
 
 # Functions for query information
 def show_all_student(student_list):
     tab_width = 20
     print("Student name" + " " * 8 + "Student ID" + " " * 10 + "Date of birth" + " " * 7)
     for student in student_list:
-        for item in student.keys():
+        for item in student.keys():  # Print item by item
             if len(item) < tab_width:
                 print(student[item] + " " * (tab_width - len(student[item])), end="")  # Formatted output
             else:
@@ -138,8 +140,9 @@ def show_all_class(class_list):
             str(item["Current"])
         )
 
+# Show each class student
 def show_students_class(student_list, class_list):
-    tab_width = 20
+    tab_width = 20  # Set the padding for the table column
     class_to_display = formatted_input("Enter the class ID to display: ",1)
     class_position = find_dict_in_list("ID", class_to_display, class_list)
     if class_position == -1:
@@ -147,22 +150,24 @@ def show_students_class(student_list, class_list):
         return
 
     print("Student name" + " " * 8 + "Student ID" + " " * 10 + "Date of birth" + " " * 7)
-    for student in class_list[class_position]["Student list"]:
-        student_position = find_dict_in_list("ID", student, student_list)
-        student_data = student_list[student_position]
-        for key in student_data.keys():
+    for student in class_list[class_position]["Student list"]:  # Get each item in the list of student's ID
+        student_position = find_dict_in_list("ID", student, student_list) # Find the item position in the student list
+        student_data = student_list[student_position]  # Reference it back to student list
+        for key in student_data.keys():  # Print the data from the reference
             print(student_data[key] + " " * (tab_width - len(student_data[key])), end="")
         print()
 
 def show_mark(student_list, class_list):
     tab_width = 20
     class_to_display = formatted_input("Enter the class ID to display: ",1)
+
     class_position = find_dict_in_list("ID", class_to_display, class_list)   
     if class_position == -1:
         input("No class found. Press Enter to try again!")
         return
+
     print("Student name" + " " * 8 + "Student ID" + " " * 10 + "Mark")
-    for student in class_list[class_position]["Mark list"]:
+    for student in class_list[class_position]["Mark list"]:  # This part is similar to the function above
         for key in student.keys():
             student_position = find_dict_in_list("ID", key, student_list)
             student_data = student_list[student_position]
@@ -172,8 +177,8 @@ def show_mark(student_list, class_list):
                 student[key]
             )
 # Initialize program data
-classes = []
-students = []
+classes = []  # Hold the list of all classes
+students = []  # Hold the list of all students
 
 while True:
     os.system("cls")
