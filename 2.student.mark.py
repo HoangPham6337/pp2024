c1685164be1f3678791451d9a1cbbed4f863f03c
HoangPham4171 <52208311+HoangPham6337@users.noreply.github.com>
2024-01-12 10:38:20 +0700

    Finish adding the student with student's data and marks

--- 2.student.mark.py
+++ 2.student.mark.py
@@ -25,7 +25,7 @@ def check_duplicate_list_obj(check_case, list_to_search, item_of_interest) -> bo
 
 def find_item_in_list(item_to_find, list_to_search):
     for i in range(0, len(list_to_search)):
-        if list_to_search[i].get_name() == item_to_find or list_to_search[i].get_id() == item_to_find:
+        if list_to_search[i].get_name().upper() == item_to_find or list_to_search[i].get_id().upper() == item_to_find:
             return i
     return -1
 
@@ -37,6 +37,7 @@ class Course:
         self.__id = id
         self.__total = total
         self.__current = 0
+        self.__students = []
 
     def __str__(self) -> str:
         return f"Class name: {self.__name}\nClass ID: {self.__id}"
@@ -54,7 +55,7 @@ class Course:
         return self.__current
 
     def modify_total_student(self, number_student):
-        if (number_student < 0):
+        if (int(number_student) <= self.__total):
             print(f"Invalid input, failed to change {self.__name} number of student")
         self.__total = number_student
     
@@ -65,7 +66,27 @@ class Course:
         if self.__current < self.__total:
             return False
         return True
+
+    def add_student_id(self, student):
+        self.__students.append(student.get_id())
     
+    def check_duplicate_student(self, student_to_add) -> bool:
+        for student in self.__students:
+            if student == student_to_add:
+                return True
+        return False
+    
+    def show_all_student(self, student_list):
+        tab_width = 20
+        print("Student name" + " " * 8 + "Student ID" + " " * 10 + "Date of birth" + " " * 7)
+        for student in self.__students:
+            student_data = student_list[find_item_in_list(self.__id, student_list)]
+            print(
+                student_data.get_name() + " " * (tab_width - len(student_data.get_name())) +
+                student_data.get_id() + " " * (tab_width - len(student_data.get_id())) +
+                student_data.get_dob()
+            )
+
 def add_course(course_list, mark_list, course_limit):
         name, id, total = "", "", ""
         if len(course_list) >= course_limit:
@@ -73,6 +94,7 @@ def add_course(course_list, mark_list, course_limit):
                 course_limit += 1
             else:
                 print("Failed to add more course.")
+                return
 
         while True:
             name = input_data("Enter the course name: ").title()
@@ -83,7 +105,7 @@ def add_course(course_list, mark_list, course_limit):
                 continue
             break
         
-        mark_list.append(Mark(id))
+        mark_list.append(Mark(name ,id))
         
         while True:
             try:
@@ -106,24 +128,32 @@ def show_all_courses(course_list):
         )
 
 class Mark:
-    def __init__(self, class_id):
-        self.__id = class_id
+    def __init__(self, name, id):
+        self.__name = name
+        self.__id = id
         self.__students_mark = []  # List of dictionary {ID: mark}
     
     def __str__(self):
         return f"This is the marking list for {self.id}"
+
+    def get_name(self) -> str:
+        return self.__name
     
     def get_id(self) -> str:
         return self.__id
 
     def add_mark(self, student_id, mark, student_list):
         if find_item_in_list(student_id, student_list) != -1:
-            if mark > 0:
+            if int(mark) > 0:
                 student_mark = {
                     "ID": student_id,
                     "Mark": mark
                 }
                 self.__students_mark.append(student_mark)
+            else:
+                print("Invalid mark. Please try again!")
+        else:
+            print("Student not found. Please try again!")
 
     def show_mark(self, student_id) -> dict:
         for student in self.__students_mark:
@@ -135,8 +165,7 @@ class Mark:
         tab_width = 20
         print("Student name" + " " * 8 + "Student ID" + " " * 10 + "Mark")
         for student in self.__students_mark:
-            student_position= find_item_in_list(self.__id, student_list)
-            student_data = student_list[student_position]
+            student_data = student_list[find_item_in_list(self.__id, student_list)]
             print(
                 student_data.get_name() + " " * (tab_width - len(student_data.get_name())) +
                 student_data.get_id() + " " * (tab_width - len(student_data.get_id())) +
@@ -146,11 +175,10 @@ class Mark:
 
 # All about students
 class Student:
-    def __init__(self, name, id, dob, student_list):
-        __name = name
-        __id = id
-        __dob = dob
-        __courses = []
+    def __init__(self, name, id, dob):
+        self.__name = name
+        self.__id = id
+        self.__dob = dob
     
     def __str__(self) -> str:
         return f"Student name: {self.name}\nStudent ID: {self.id}"
@@ -161,6 +189,9 @@ class Student:
     def get_id(self) -> str:
         return self.__id
     
+    def get_dob(self) -> str:
+        return self.__dob
+    
     def set_courses(self, new_course):
         self.__courses.append(new_course)
 
@@ -168,15 +199,52 @@ class Student:
         return self.__courses
     
 
-def add_student(student_list, course_list):
+def add_student(student_list, course_list, mark_list):
+    name, id, dob = "", "", ""
     while True:
         name = input_data("Enter student name: ").title()
-        if not check_duplicate_list_obj(name, student_list, "student"):
-            break
+        if check_duplicate_list_obj(name, student_list, "student"):
+            continue
         id = input_data("Enter student ID: ").upper()
-        if not check_duplicate_list_obj(id, student_list, "student"):
+        if check_duplicate_list_obj(id, student_list, "student"):
+            continue
+        break
+    while True:
+        dob = input_data(f"Enter {name} date of birth (DD-MM-YYYY): ").strip()
+        try:
+            # Convert the input to date type and format it
+            dob = datetime.strptime(dob, "%d-%m-%Y").date().strftime("%d-%m-%Y")
             break
-        
+        except ValueError:
+            print("Invalid date-time input. Please try again!")
+    newStudent = Student(name, id, dob)
+    student_list.append(newStudent)
+
+    while True:
+        course_to_add = input_data("Enter the course to add student: ").upper()
+        course_position = find_item_in_list(course_to_add, course_list)
+        if course_position == -1:
+            continue
+
+        course_data = course_list[course_position]
+        if course_data.is_full():
+            if input(f"{course_data.get_name()} is full, do you want to increase the total number of student? (Y/N) ").upper() == 'Y':
+                increase = input(f"Current: {str(course_data.get_total_student())} -> Change to: ")
+                course_data.modify_total_student(increase)
+            else:
+                input("Failed to add student, press Enter to continue.")
+        if course_data.check_duplicate_student(newStudent):
+            print("Duplicate student found in the class, please try again!")
+            continue
+        course_data.add_student_id(newStudent)
+        course_data.add_current_student()
+
+        mark_position = find_item_in_list(course_to_add, mark_list)
+        mark_data = mark_list[mark_position]
+        mark_data.add_mark(id, input("Mark: "), student_list)
+        break
+    
+    
     
 
 
@@ -200,6 +268,7 @@ while True:
     )
     match choice:
         case "1":
+            add_student(students, courses, marks)
             os.system("cls")
         case "2":
             add_course(courses, marks, course_limit)
