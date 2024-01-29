5a565f17b0484d1d4813ec134963127cad65d406
HoangPham4171 <52208311+HoangPham6337@users.noreply.github.com>
2024-01-29 19:50:35 +0700

    Finsh labwork 4

---
+++ pw4/domains/StudentMark.py
@@ -0,0 +1,18 @@
+from .Student import Student
+
+
+class StudentMark(Student):
+    def __init__(self, name, id, dob):
+        super().__init__(name, id, dob)
+        self.__mark = 0
+
+    def get_mark(self) -> int:
+        return self.__mark
+
+    def set_mark(self, mark) -> int:
+        if mark < 0:
+            print("Invalid input, please try again!")
+            return -1
+        else:
+            self.__mark = mark
+            return 0
